e29fe989ebb1685dcfd7624c25b4cc100b5999e9
Leo <leonardonifinluri@gmail.com>
2025-07-19 09:21:08 +0800

    refactor: remove wrong component in admin layout

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,27 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export default function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <main>{children}</main>
-      </body>
-    </html>
-  );
+  return <main>{children}</main>;
 }
