0dc2dcc89a6476bbbdbe442b18ad8f7174ed16e0
Leo <leonardonifinluri@gmail.com>
2025-07-19 12:22:08 +0800

    fix: fix middleware problem that let anyone see the admin routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,23 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-// lis of protected routes
-const protectedRoutes = ["/admin"];
-
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
-  const isProtectedRoute = protectedRoutes.some((route) => pathname.startsWith(route));
-  if (isProtectedRoute) return NextResponse.next();
-  
-  // check for session token
   const token = request.cookies.get("__session")?.value;
 
-  // if no token and trying to access protected route, redirect to login
-  if (!token) {
-    return NextResponse.redirect(new URL("/admin/login", request.url))
+  const isProtectedRoute = pathname.startsWith("/admin");
+
+  if (isProtectedRoute && !token) {
+    console.log("⛔ No session token, redirecting to /login");
+    return NextResponse.redirect(new URL("/login", request.url));
   }
 
+  // Allow through if:
+  // - route is not protected
+  // - or protected route with valid token
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/admin/:path*"], // apply middleware to all admin routes (prefix /admin)
+  matcher: ["/admin/:path*"], // apply middleware to all /admin routes
 };
