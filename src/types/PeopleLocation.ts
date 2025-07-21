8f27976d175467010bbd8b032ee806e9f04e1911
Leo <leonardonifinluri@gmail.com>
2025-07-21 15:59:31 +0800

    feat: change type from document reference to string

--- src/types/PeopleLocation.ts
+++ src/types/PeopleLocation.ts
@@ -1,10 +1,10 @@
-import { DocumentReference, GeoPoint } from "firebase/firestore";
+import { GeoPoint } from "firebase/firestore";
 
 export interface PeopleLocation {
   id: string;
   name: string;
   contact_number: string;
   location: GeoPoint;
-  work_as: DocumentReference;
-  hamlet: DocumentReference;
+  work_as: string;
+  hamlet: string;
 }
