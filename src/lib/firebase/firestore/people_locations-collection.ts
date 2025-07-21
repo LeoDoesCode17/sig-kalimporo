35b9dbd91c3a376de629045f4981779c111d64f2
Leo <leonardonifinluri@gmail.com>
2025-07-21 16:02:02 +0800

    feat: add function to get occupation and hamlet from a document reference

--- src/lib/firebase/firestore/people_locations-collection.ts
+++ src/lib/firebase/firestore/people_locations-collection.ts
@@ -1,19 +1,56 @@
-import { collection, getDocs } from "firebase/firestore";
+import {
+  collection,
+  DocumentReference,
+  getDoc,
+  getDocs,
+} from "firebase/firestore";
 import { firestore } from "../config";
 import { PeopleLocation } from "@/types/PeopleLocation";
+import { Occupation } from "@/types/Occupation";
+import { Hamlet } from "@/types/Hamlet";
 
 const peopleRef = collection(firestore, "people_locations");
 
 export const PeopleLocationService = {
   async getAll(): Promise<PeopleLocation[]> {
     const querySnapshot = await getDocs(peopleRef);
-    return querySnapshot.docs.map((doc) => ({ 
-      id: doc.id,
-      name: doc.data().name,
-      contact_number: doc.data().contact_number,
-      location: doc.data().location,
-      work_as: doc.data().work_as,
-      hamlet: doc.data().hamlet
-    }));
-  }
-}
+
+    const peoplePromises = querySnapshot.docs.map(async (doc) => {
+      const data = doc.data();
+      const occupation = await this.getOccupation(
+        data.work_as as DocumentReference
+      );
+      const hamlet = await this.getHamlet(data.hamlet as DocumentReference);
+
+      return {
+        id: doc.id,
+        name: data.name,
+        contact_number: data.contact_number,
+        location: data.location,
+        work_as: occupation?.name ?? "Tidak diketahui",
+        hamlet: hamlet?.name ?? "Tidak diketahui",
+      };
+    });
+    return Promise.all(peoplePromises);
+  },
+  async getOccupation(work_as: DocumentReference): Promise<Occupation | null> {
+    const occupationSnapshot = await getDoc(work_as);
+    if (!occupationSnapshot.exists()) {
+      return null;
+    }
+    return {
+      id: occupationSnapshot.id,
+      name: occupationSnapshot.data().name,
+    };
+  },
+  async getHamlet(hamlet: DocumentReference): Promise<Hamlet | null> {
+    const hamletSnapshot = await getDoc(hamlet);
+    if (!hamletSnapshot.exists()) {
+      return null;
+    }
+    return {
+      id: hamletSnapshot.id,
+      name: hamletSnapshot.data().name,
+    };
+  },
+};
