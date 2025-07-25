3bf9ea1a84ba9044a8ca58b0a1904579d0ef5208
Leo <leonardonifinluri@gmail.com>
2025-07-25 13:44:10 +0800

    feat: add onUpdateSuccess in leaflet map props

--- src/types/LeafletMapProps.ts
+++ src/types/LeafletMapProps.ts
@@ -3,4 +3,5 @@ import { PeopleLocation } from "./PeopleLocation";
 export interface LeafletMapProps {
   people_locations: PeopleLocation[];
   onDeleteSuccess?: () => Promise<void>;
+  onUpdateSuccess?: () => Promise<void>;
 }
